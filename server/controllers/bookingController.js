e4506a17399c72934f3bcb5b4c221299559adac0
Aarsh Singh <starfire9599@gmail.com>
2026-03-26 00:56:17 +0530

    feat: API to list user Bookings

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -75,7 +75,20 @@ export const createBooking = async (req, res) => {
   }
 };
 
+// API to list user Bookings
+export const getUserBookings = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const bookings = (await Booking.find({ user: _id }).populate("car")).sort({
+      createdAt: -1,
+    });
 
+    res.json({ success: true, message: bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 
 
