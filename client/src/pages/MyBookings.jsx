a54be4733a2eb9bc043621030e717559d28adc23
Aarsh Singh <starfire9599@gmail.com>
2026-03-28 00:34:06 +0530

    feat: Fetched user bookings from DB and add animations

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
-import { assets, dummyMyBookingsData } from "../assets/assets";
+import { assets } from "../assets/assets";
 import Title from "../components/Title";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
+import { motion } from "motion/react";
 
 const MyBookings = () => {
+  const { axios, user, currency } = useAppContext();
+
   const [bookings, setBookings] = useState([]);
-  const currency = import.meta.env.VITE_CURRENCY;
 
   const fetchMyBookings = async () => {
-    setBookings(dummyMyBookingsData);
+    try {
+      const { data } = await axios.get("/api/bookings/user");
+
+      if (data.success) {
+        setBookings(data.bookings);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
-    fetchMyBookings();
-  }, []);
+    user && fetchMyBookings();
+  }, [user]);
 
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+    <motion.div
+      initial={{ y: 30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6 }}
+      className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl"
+    >
       <Title
         title="My Bookings"
         subTitle="View and manage your all car bookings"
@@ -24,7 +43,10 @@ const MyBookings = () => {
 
       <div>
         {bookings.map((booking, index) => (
-          <div
+          <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, delay: index *  0.1 }}
             key={booking._id}
             className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12"
           >
@@ -89,17 +111,20 @@ const MyBookings = () => {
             </div>
 
             {/* Price */}
-            <div className="md:col-span-1 flex- flex-col justify-between gap-6">
+            <div className="md:col-span-1 flex flex-col justify-between gap-6">
               <div className="text-sm text-gray-500 text-right">
                 <p>Total Price</p>
-                <h1 className="text-2xl text-primary font-semibold">{currency}{booking.price}</h1>
-                <p>Booked on {booking.createdAt.split('T')[0]}</p>
+                <h1 className="text-2xl text-primary font-semibold">
+                  {currency}
+                  {booking.price}
+                </h1>
+                <p>Booked on {booking.createdAt.split("T")[0]}</p>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
